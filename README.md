8afbe24b4650297a453837aba5d25382f4337f2c
jpsirois <email@jpsirois.com>
2014-01-20 12:21:01 -0500

    Inital commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# Patterns Browser
+
+A quickly put up simple web view to have a quick and easy way to browse Patterns.
+
+## Patterns Availanle
+* The 356 first [SubtlePatterns](http://subtlepatterns.com/)
+* Transparent variants of 154 SubtlePatterns made by @halgatewood
+
+## Features
+* Grid view of all the Patterns, by library
+* Fullscreen background view of all the Patterns, by library
+
+## TODOS
+
+* Add a background-color changer for the transparent variation
+* Update with the all the latest [Subtle Patterns](http://subtlepatterns.com/) submissions. Right now contain all the Pattern from their not updated anymore [GitHub Repository](https://github.com/subtlepatterns/SubtlePatterns). [Last update](https://github.com/subtlepatterns/SubtlePatterns/commit/6e056565a1254c967228aab60c9d9c7d0e63a7e3) on 2013-08-06.
+* Clean SCSS & HAML structure, I used stuff from another of my project and it may have some useless things for the current project purpose.
+* Add the metadata for each Pattern if available
+* Add more Patterns library
+* Add more views
+
+## Thank you
+
+* @subtlepatterns for all those quality Patterns & for the [demo.html](https://github.com/subtlepatterns/SubtlePatterns/blob/gh-pages/demo.html) JavaScript example using the GitHub API to easily access assets.
+* @halgatewood for providing [transparent variations of 150 Subtle Pattern](http://halgatewood.com/150-transparent-subtle-patterns/)
+
+## License
+
+© 2014 licensed under a [MIT license](http://jpsirois.mit-license.org/license.html)
+
+
+
+
---
+++ source/js/scripts.js
@@ -0,0 +1,156 @@
+(function ($, window, document, undefined) {
+
+  'use strict';
+
+  /* jQuery SmartResize {{{
+   * Source: http://www.paulirish.com/2009/throttled-smartresize-jquery-event-handler/ */
+
+  // debouncing function from John Hann
+  // http://unscriptable.com/index.php/2009/03/20/debouncing-javascript-methods/
+  var debounce = function (func, threshold, execAsap) {
+    var timeout
+
+    return function debounced () {
+      var obj = this, args = arguments
+      function delayed () {
+        if (!execAsap) {
+          func.apply(obj, args)
+        }
+        timeout = null
+      }
+
+      if (timeout) {
+        clearTimeout(timeout)
+      }
+      else if (execAsap) {
+        func.apply(obj, args)
+      }
+
+      timeout = setTimeout(delayed, threshold || 100)
+    }
+  }
+  // smartresize
+  jQuery.fn.smartresize = function(fn){  return fn ? this.bind('resize', debounce(fn)) : this.trigger('smartresize') }
+
+  /*}}}*/
+
+  /* Boxes Layout {{{
+   * Inspiration: http://benholland.me/javascript/how-to-build-a-site-that-works-like-pinterest/ */
+
+  var colCount = 0,
+      colWidth = 0,
+      margin = 12,
+      spaceLeft = 0,
+      windowWidth = 0,
+      boxes = [],
+      boxWrapSelector = '.box'
+
+  $(window).load(function(){
+    setupBoxes()
+    $(window).resize(setupBoxes)
+  })
+
+  function setupBoxes() {
+    windowWidth = $(window).width()
+    boxes = []
+    colWidth = $(boxWrapSelector).outerWidth()
+    colCount = Math.floor(windowWidth/(colWidth+margin*2))
+    spaceLeft = (windowWidth - ((colWidth*colCount)+(margin*(colCount-1)))) / 2
+    for(var i=0;i<colCount;i++){
+      boxes.push(margin)
+    }
+    positionBoxes()
+  }
+
+  function positionBoxes() {
+    $(boxWrapSelector).each(function(){
+      var min = Array.min(boxes),
+          index = $.inArray(min, boxes),
+          leftPos = margin+(index*(colWidth+margin))
+
+      $(this).css({
+        'left':(leftPos+spaceLeft)+'px',
+        'top':min+'px',
+        'visibility': 'visible'
+      })
+
+      boxes[index] = min+$(this).outerHeight()+margin
+    })
+  }
+
+  // Function to get the Min value in Array
+  Array.min = function(array) {
+    return Math.min.apply(Math, array)
+  }
+
+  /*}}}*/
+
+  /* Item Handling {{{*/
+
+  $('.itemWrap').each(function(){
+    var $this = $(this)
+
+    /* Add itemWrap .focus class on itemLink focus {{{*/
+
+    $this.find('.itemLink').on('keyup',function(e) {
+      if ( e.which === 9 ) {
+        $this.addClass('focus')
+      }
+    })
+    $this.find('.itemLink').on('blur', function() {
+      $this.removeClass('focus')
+    })
+
+    /*}}}*/
+
+  })
+
+  /*}}}*/
+
+})(jQuery, window, document)
+
+
+// Use GitHub as API to Load patterns
+// Inspired from: https://github.com/subtlepatterns/SubtlePatterns/blob/gh-pages/demo.html
+var loadPatterns = function () {
+  var ajax, ajaxTimeout, requrl;
+  ajax = (window.ActiveXObject) ? new ActiveXObject("Microsoft.XMLHTTP") : (XMLHttpRequest && new XMLHttpRequest()) || null;
+
+  ajaxTimeout = window.setTimeout(function () {
+    ajax.abort();
+  }, 6000);
+
+  ajax.onreadystatechange = function () {
+    var selectEle, itemsArray, itemsString, i;
+    if (ajax.readyState === 4) {
+      if (ajax.status === 200) {
+        clearTimeout(ajaxTimeout);
+        if (ajax.status !== 200) {
+
+        } else {
+          selectEle = document.getElementById('patterns-selector');
+          itemsArray = JSON.parse(ajax.responseText);
+          itemsString = '';
+          for(i = 0; i < itemsArray.length; i++) {
+            if(itemsArray[i].name.indexOf('.png') !== -1) {
+              itemsString += '<option value="'+itemsArray[i].html_url.replace("blob", "raw")+'">'+itemsArray[i].name+'</option>';
+            }
+          }
+          selectEle.innerHTML = itemsString;
+          selectEle.setAttribute("onchange", "setPattern(this.value);"); // = setPattern(selectEle.value);
+        }
+      }
+    }
+  };
+
+  requrl = 'https://api.github.com/repos/jpsirois/patterns-browser/git/trees/master?recursive=1';
+  ajax.open("GET", requrl, true);
+  ajax.send();
+}
+
+var setPattern = function (url) {
+  var bodyEle = document.getElementsByTagName('body')[0];
+  bodyEle.style.backgroundImage = 'url("'+url+'")';
+}
+
+window.onload = loadPatterns();
